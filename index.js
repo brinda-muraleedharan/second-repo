a506be5bd1faffab9de99bb48813321aec9a2111
Brinda Muraleedharan <brinda.salesforce@gmail.com>
2020-10-15 16:24:17 -0400

    clonning methods

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-the new line
+// cloning
+//USing for loop and iterating
+const nikiphone={
+    phNo:7063995797,
+    brand:'Mi A1', 
+}
+const brindaPhone={}
+for(let key in nikiphone){
+    brindaPhone[key]=nikiphone[key];
+} 
+const newObejetc={
+    newProperty:1234
+}
+const bruPhone={phNo:9962237637};
+Object.assign(bruPhone,nikiphone);
+
+const chinnuPhone={...nikiphone};
